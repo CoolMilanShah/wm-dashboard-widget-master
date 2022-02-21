800aa2d1dc2431356b3dbe8ef73e2cdd1cf16bba
Dhaval A Shah <Dhaval456Shah2002@yahoo.com>
2022-02-21 18:08:00 +0530

    Updates

---
+++ precache-manifest.fc8b8b9fa75d72c0047087210f0c20f5.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b81ee2b8b2231668b7d80394a12eaf3",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/index.html"
+  },
+  {
+    "revision": "dda733f4e8ae445ee8c7",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/10.2a60accc.chunk.css"
+  },
+  {
+    "revision": "ab036814edb7bdc7b1eb",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/11.2a60accc.chunk.css"
+  },
+  {
+    "revision": "ad64b28262bea2e02d08",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/12.ade3ff96.chunk.css"
+  },
+  {
+    "revision": "0e7039d0a3896ab10383",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/3.a5d6d810.chunk.css"
+  },
+  {
+    "revision": "de85a7d5983abdd46d73",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/6.2a60accc.chunk.css"
+  },
+  {
+    "revision": "f6579a9dc43285f5b0ec",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/7.2a60accc.chunk.css"
+  },
+  {
+    "revision": "b06ef4ab9e0eb0d950bb",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/8.2a60accc.chunk.css"
+  },
+  {
+    "revision": "bc25259c239efd00ce8d",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/9.2a60accc.chunk.css"
+  },
+  {
+    "revision": "633e394b60979e58f914",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/css/main.4b0caf6f.chunk.css"
+  },
+  {
+    "revision": "e560a3f768fc81299970",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/0.434b9106.chunk.js"
+  },
+  {
+    "revision": "ba032f7a1c8b1841399b",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/1.318ce4d8.chunk.js"
+  },
+  {
+    "revision": "dda733f4e8ae445ee8c7",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/10.ff40af27.chunk.js"
+  },
+  {
+    "revision": "ab036814edb7bdc7b1eb",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/11.bbb90304.chunk.js"
+  },
+  {
+    "revision": "ad64b28262bea2e02d08",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/12.c943fb91.chunk.js"
+  },
+  {
+    "revision": "b148e81b4b28b3d525d3",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/13.841adb88.chunk.js"
+  },
+  {
+    "revision": "e9f4148863dd40ba7a60",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/14.bc803890.chunk.js"
+  },
+  {
+    "revision": "b9d8f7c5d8310fa4f6c5",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/15.996a7d59.chunk.js"
+  },
+  {
+    "revision": "7628e682b65df5a2a84c",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/16.98b21870.chunk.js"
+  },
+  {
+    "revision": "ef4f4a2ea076a41ea49d",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/17.f5cca331.chunk.js"
+  },
+  {
+    "revision": "dbe4530f51622686ebe7",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/18.ee5ecfaa.chunk.js"
+  },
+  {
+    "revision": "8ff13324244ba370f6b5",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/19.fc10d599.chunk.js"
+  },
+  {
+    "revision": "196f2f5a5d458f5122d4",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/2.4ab31052.chunk.js"
+  },
+  {
+    "revision": "c48d9d60dcf64b026ed6",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/20.ffca6adf.chunk.js"
+  },
+  {
+    "revision": "5d9dd6996c622b6eb36c",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/21.97034487.chunk.js"
+  },
+  {
+    "revision": "0e7039d0a3896ab10383",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/3.d5cfc3e7.chunk.js"
+  },
+  {
+    "revision": "de85a7d5983abdd46d73",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/6.0f6cb118.chunk.js"
+  },
+  {
+    "revision": "f6579a9dc43285f5b0ec",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/7.abade80b.chunk.js"
+  },
+  {
+    "revision": "b06ef4ab9e0eb0d950bb",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/8.16bae72a.chunk.js"
+  },
+  {
+    "revision": "bc25259c239efd00ce8d",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/9.f8b70b78.chunk.js"
+  },
+  {
+    "revision": "633e394b60979e58f914",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/main.18009ae2.chunk.js"
+  },
+  {
+    "revision": "11cfd8a0de34186896ee",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/js/runtime-main.e6419414.js"
+  },
+  {
+    "revision": "c31ee675e2b37072656ad8fd122c74bf",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/media/logo.c31ee675.png"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "/CoolMilanShah/wm-dashboard-widget-master/static/media/sidebar-4.80d4a4e5.jpg"
+  }
+]);
